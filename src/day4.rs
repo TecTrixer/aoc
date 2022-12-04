788eec70e9c18215f2b4352a7f439ec072bcc368
TecTrixer <tonihoevedes@gmail.com>
2022-12-04 07:01:03 +0100

    beautified code + better parsing

--- src/day4.rs
+++ src/day4.rs
@@ -1,29 +1,17 @@
 use cp_rs::io::*;
 fn main() {
     let mut io = Io::from_file("day4.txt");
-
     let mut res1 = 0;
     let mut res2 = 0;
-    for line in io.lines() {
-        let mut split = line.split(',');
-        let first = split.next().unwrap();
-        let mut range1 = first.split('-');
-        let t1 = range1.next().unwrap().parse::<usize>().unwrap();
-        let t2 = range1.next().unwrap().parse::<usize>().unwrap();
-        let sec = split.next().unwrap();
-        let mut range2 = sec.split('-');
-        let t3 = range2.next().unwrap().parse::<usize>().unwrap();
-        let t4 = range2.next().unwrap().parse::<usize>().unwrap();
-
-        if t1 <= t3 && t2 >= t4 || t3 <= t1 && t4 >= t2 {
+    for r in io.nums::<isize>().chunks(4) {
+        let fl = r[0];
+        let fr = r[1] * -1;
+        let sl = r[2];
+        let sr = r[3] * -1;
+        if fl <= sl && fr >= sr || fl >= sl && fr <= sr {
             res1 += 1;
         }
-
-        if t2 >= t4 {
-            if t1 <= t4 {
-                res2 += 1;
-            }
-        } else if t2 >= t3 {
+        if fl <= sl && fr >= sl || fl >= sl && fl <= sr {
             res2 += 1;
         }
     }
